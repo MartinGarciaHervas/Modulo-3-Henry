635bf81f4e34e5c499270f237fe9b1081e328c8f
Martin Garcia Hervas <punighervas@gmail.com>
2023-09-04 15:54:20 -0300

    Finished

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,10 +2,22 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write("prompt > ");
+   process.stdin.on("data", (data)=>{
+      let args = String(data).trim();
+      let cmd = args.split(' ')[0];
+      commands.hasOwnProperty(cmd)?commands[cmd](print, args.substring(5)):print(`command not found: ${cmd}`);
+   })
+}
 
 bash();
 module.exports = {
    print,
    bash,
 };
+
+function print (output) {
+   process.stdout.write(output);
+   process.stdout.write("\nprompt > ")
+}
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,53 @@
 const fs = require("fs");
-const utils = require("../utils/request");
+const utils = require("../utils/request.js");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args)
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir('.', (error, files)=>{
+        if (error) throw error;
+        print(files.join(' '))
+    })
+}
 
-function cat() {}
+function cat(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if(error) throw error;
+        print(data);
+    })
+}
 
-function head() {}
+function head(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if(error)  throw error;
+        print(data.split('\n')[0]); // split('\n') lo divide en lineas, y mediante el [0] accedo a la primera
+    })
+}
 
-function tail() {}
+function tail(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if (error) throw error;
+        let enLineas = data.split('\n')
+        print(enLineas[enLineas.length - 1].trim());
+    })
+}
 
-function curl() {}
+function curl(print, args) {
+    utils.request(args, (error, response)=>{
+        if(error) throw error;
+        print(response);
+    })
+}
 
-module.exports = {};
+module.exports = {pwd, date, echo, ls, cat, head, tail, curl};
