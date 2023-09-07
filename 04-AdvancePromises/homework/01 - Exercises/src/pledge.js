547924a71fed1eae57e592b1895a71e9531e503c
Martin Garcia Hervas <punighervas@gmail.com>
2023-09-07 18:35:42 -0300

    Promesas Avanzadas

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -4,6 +4,35 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+function $Promise(executor) {
+    if (typeof executor !== 'function') { throw new TypeError('executor its not a function') }
+    
+    this._state = 'pending';
+
+    const contexto = this
+    
+    const resolve = function(data){
+        contexto._state = 'fulfilled';
+        contexto._value = data;
+    }
+    const reject = function(){}
+
+    executor(resolve, reject);
+}
+
+$Promise.prototype._internalResolve = function (data) {
+    if (this._state === 'pending') {
+        this._value = data;
+        this._state = 'fulfilled';
+    }
+};
+$Promise.prototype._internalReject = function (reason) {
+    if (this._state === 'pending') {
+        this._value = reason;
+        this._state = 'rejected';
+    }
+};
+
 
 
 module.exports = $Promise;
